37687e5a910119c5932a860aeeec0ff60fb634ed
shafrazi <shafrazi@live.com>
2020-10-05 00:12:46 +0530

    Updates

---
+++ precache-manifest.e87043324f2fa8ae37a37bfb19a4c7a4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4cbabc84429b2f5e4d8e450197ee5f5",
+    "url": "/memory-cards/index.html"
+  },
+  {
+    "revision": "a7ddf425677c9bb0f761",
+    "url": "/memory-cards/static/css/main.627be880.chunk.css"
+  },
+  {
+    "revision": "938a2442d21cbc7efcbf",
+    "url": "/memory-cards/static/js/2.624f2c5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-cards/static/js/2.624f2c5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7ddf425677c9bb0f761",
+    "url": "/memory-cards/static/js/main.0242143e.chunk.js"
+  },
+  {
+    "revision": "7ccac35c08681eef11df",
+    "url": "/memory-cards/static/js/runtime-main.96810f88.js"
+  },
+  {
+    "revision": "d760bf0a7f896aa2db76158d53448c8f",
+    "url": "/memory-cards/static/media/BadaboomBB_Reg.d760bf0a.otf"
+  },
+  {
+    "revision": "e0488473551335d86a2fa9d40b88f2c2",
+    "url": "/memory-cards/static/media/adamwarrenpro-bold.e0488473.ttf"
+  },
+  {
+    "revision": "b3431dba10e27f650ab860da2775b426",
+    "url": "/memory-cards/static/media/adamwarrenpro.b3431dba.ttf"
+  }
+]);
